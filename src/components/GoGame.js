f50361f479a1db9a167b4cb53398d18496fbb0c3
chen.cpx <chen.cpx@alibaba-inc.com>
2015-08-13 21:40:02 +0800

    优化棋盘数据结构

--- src/components/GoGame.js
+++ src/components/GoGame.js
@@ -8,45 +8,48 @@ require('styles/GoGame.less');
 var GoGame = React.createClass({
   mixins: [],
   getInitialState: function() {
+    var initboard = [];
+    for (var i = 0; i < this.props.row; i++) {
+      initboard[i] = [];
+      for (var j = 0; j < this.props.col; j++) {
+        initboard[i][j] = 0;
+      }
+    }
     return {
-      borad: this.props.testborad,
+      board: initboard,
       flag: true
     };
   },
   getDefaultProps: function() {
     return {
-      testborad: [
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0
-      ]
+      row: 8,
+      col: 8
     };
   },
   onClick: function (elem) {
-    console.log(this.props.testborad);
-    var index = elem.key;
-    var newBorad = this.state.borad;
+    var newBoard = this.state.board;
+    console.dir(newBoard);
+    var x = elem.x;
+    var y = elem.y;
+    console.log(newBoard[x][y]);
     if (!elem.item) {
-      newBorad[index] = this.state.flag ? 'Ⅹ' : 'O';
+      newBoard[x][y] = this.state.flag ? 'Ⅹ' : 'O';
       this.setState({
-        borad: newBorad,
+        board: newBoard,
         flag: !this.state.flag
       });
     }
   },
-  clearBorad: function () {
-    var newBorad = this.state.borad;
-    newBorad.forEach(function (elem, index) {
-      newBorad[index] = 0;
-    });
-    console.dir(newBorad);
+  clearBoard: function () {
+    var initboard = [];
+    for (var i = 0; i < this.props.row; i++) {
+      initboard[i] = [];
+      for (var j = 0; j < this.props.col; j++) {
+        initboard[i][j] = 0;
+      }
+    }
     this.setState({
-      borad: newBorad,
+      board: initboard,
       flag: this.state.flag
     });
   },
@@ -57,24 +60,29 @@ var GoGame = React.createClass({
   componentWillUnmount: function() {},
 
   render: function () {
-    var GameBtn = this.state.borad.map(function(elem, index) {
-      var showText = '十';
-      if (elem) {
-        showText = elem;
-      }
-      return <div key={index} onClick={this.onClick.bind(this, {key: index, item: elem})} className="GameBtn">{showText}</div>;
-    }.bind(this));
+    var self = this;
+    var GameBtn = this.state.board.map(function(item, x) {
+      return item.map(function(elem, y) {
+        var showText = '十';
+        if (elem) {
+          showText = elem;
+        }
+        return <div  onClick={self.onClick.bind(self, {x: x, y: y, item: elem})} className="GameBtn">{showText}</div>;
+      }.bind(self));
+
+      });
     return (
         <div className="GoGame container">
           <div className="col-md-offset-3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
             <div className="panel panel-primary">
                 <div className="panel-heading">
                   <h3 className="panel-title">Content for GoGame</h3>
-                  <button type="button" onClick={this.clearBorad} className="btn btn-default">重新开始</button>
+                  <button type="button" onClick={this.clearBoard} className="btn btn-default">重新开始</button>
                 </div>
 
                 <div className="panel-body">
                 {GameBtn}
+
                 </div>
             </div>
           </div>
